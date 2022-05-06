6ea0ccf018ed36e6c3bc478d974f1cfb5751e342
Piotr Stapp <ptrstpp950@gmail.com>
2022-05-06 14:07:42 +0200

    Add elsa to template with simple database

---
+++ ElsaOrchestrator/ElsaOrchestrator/Startup.cs
@@ -0,0 +1,75 @@
+using Bootloader.AspNet.Extensions;
+using Elsa;
+using Elsa.Persistence.EntityFramework.Core.Extensions;
+using Elsa.Persistence.EntityFramework.Sqlite;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ElsaOrchestrator
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        private IWebHostEnvironment Environment { get; }
+        private IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var elsaSection = Configuration.GetSection("Elsa");
+
+            // Elsa services.
+            services
+                .AddElsa(elsa => elsa
+                    .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
+                    .AddConsoleActivities()
+                    .AddHttpActivities(options =>
+                    {
+                        //Optional set base path
+                        //options.BasePath = "/workflows";
+                    })
+                    .AddQuartzTemporalActivities()
+                    .AddWorkflowsFrom<Startup>()
+                );
+
+            // Elsa API endpoints.
+            services.AddElsaApiEndpoints();
+
+            // For Dashboard.
+            services.AddRazorPages();
+            
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+                .UseStaticFiles() // For Dashboard.
+                .UseHttpActivities()
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    if (!Configuration.IsBootloaderHosting())
+                        endpoints.AddHealthCheckConfig();
+                    // Elsa API Endpoints are implemented as regular ASP.NET Core API controllers.
+                    endpoints.MapControllers();
+                    
+                    // For Dashboard.
+                    endpoints.MapFallbackToPage("/_Host");
+                });
+        }
+    }
+}
